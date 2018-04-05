f2702385548187046ac261c668a6134459b27092
king <287274779@qq.com>
2018-04-05 20:49:52 +0800

    update database

--- html_demo.py
+++ html_demo.py
@@ -8,6 +8,16 @@ app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:15370040.lJ@127.0.0.1/test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
 
+class role(db.Model):
+    __tablename__='roles'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(16),unique=True)
+
+class user(db.Model):
+    __tablename__='users'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(16),unique=True)
+    role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
 
 @app.route('/')
 def hello_world():
@@ -22,4 +32,6 @@ def login():
         return 'seccess'
 
 if __name__ == '__main__':
+    db.drop_all()
+    db.create_all()
     app.run(debug=True)
